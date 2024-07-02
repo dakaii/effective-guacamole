66179b46b428c3172c3faaf7c222f8782c3e9872
Daiki Nakashita <dnakashita@ovice.co>
2024-07-02 16:46:59 +0900

    wip

--- tests/businesses/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from pytest_factoryboy import register
-from tests.businesses.factories import ProductFactory
-
-from .factories import AffiliatedPersonFactory, OrganizationFactory
-
-register(AffiliatedPersonFactory)
-register(OrganizationFactory)
-register(ProductFactory)
